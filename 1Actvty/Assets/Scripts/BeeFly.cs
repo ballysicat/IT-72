e80767afe9bbe17c3b7cfefbfb76df8fa63f84f4
BallySicat <88377731+ballysicat@users.noreply.github.com>
2021-10-13 17:50:22 +0800

    ActivityUpload

---
+++ 1Actvty/Assets/Scripts/BeeFly.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeFly : MonoBehaviour
+{
+    private float speed = 2f;
+
+    //Moves this GameObject 2 units a second in the forward direction
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        speed = speed * -2;
+        Debug.Log("hit");
+    }
+}
---
+++ 1Actvty/Assets/Scripts/EnemyReaction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyReaction : MonoBehaviour
+{
+    private float speed = 1f;
+    private Vector2 target;
+    private Vector2 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = new Vector2(-7.0f,0);
+        position = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, target, step);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        target = new Vector2(10.0f,0);
+    
+    }
+}
