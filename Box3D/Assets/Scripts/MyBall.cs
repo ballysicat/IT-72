3265862d7f245ada614428ea944380b8409925ee
BallySicat <88377731+ballysicat@users.noreply.github.com>
2021-10-14 18:51:13 +0800

    Box3D

---
+++ Box3D/Assets/Scripts/MyBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBall : MonoBehaviour
+{
+    private float speed = 2f;
+
+    //Moves this GameObject 2 units a second in the forward direction
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+    }
+
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        speed = speed * -2;
+    }
+}
