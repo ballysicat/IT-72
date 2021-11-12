fe8c4abf81882dee12f7df12533e1f5bcb2e286e
BallySicat <88377731+ballysicat@users.noreply.github.com>
2021-11-12 09:19:38 +0800

    Update

---
+++ RunningMan/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+   public GameObject target;
+   public GameObject canvas;
+   UnityEngine.AI.NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position; 
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene("Scene1");
+        }
+    }
+}
---
+++ RunningMan/Assets/Scripts/PLayBtnScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayBtnScript : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject enemy;
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.SetActive(false);
+        enemy.SetActive(false);
+        
+        
+    }
+
+    public void onClickPlay()
+    {
+        player.SetActive(true);
+        enemy.SetActive(true);
+        canvas.SetActive(false);
+    }
+
+
+}
